935744e38221457aba80f892271edc5a41427b84
Mercan Karacabey <mercankrcby@gmail.com>
2018-01-08 09:49:18 +0300

    FinalProjectGeneralStructure Completed
    Complete the algorithm

---
+++ src/com/mercankrcby/MatchFlights.java
@@ -0,0 +1,17 @@
+package com.mercankrcby;
+
+import java.util.ArrayList;
+
+/**
+ * Created by macboookair on 1.01.2018.
+ */
+public class MatchFlights {
+    public ArrayList<FlightData> flightDataList;
+    public MatchFlights(ArrayList<FlightData> fDataList){
+        flightDataList=new ArrayList<FlightData>();
+        flightDataList=fDataList;
+    }
+    public void createFlightLists(){
+
+    }
+}
